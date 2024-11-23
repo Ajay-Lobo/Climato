986d136ed2fb345102cf5979eaea79d9234f932d
Ajay-Lobo <nishanthajay1101@gmail.com>
2024-11-23 23:44:41 +0530

    tanstack query and tanstack query devtools setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 
 import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import { ThemeProvider } from './context/theme-provider'
 import WeatherDashboard from './pages/weather-dashboard'
 import CityPage from './pages/city-page'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient()
+
 function App() {
  
 
   return (
+    <QueryClientProvider client={queryClient}>
+
    <BrowserRouter>
    <ThemeProvider defaultTheme='dark'>
    <Layout>
@@ -18,7 +24,10 @@ function App() {
     </Routes>
    </Layout>
    </ThemeProvider>
-   </BrowserRouter> 
+   </BrowserRouter>
+   <ReactQueryDevtools initialIsOpen={false} />
+   </QueryClientProvider>
+ 
   )
 }
 
