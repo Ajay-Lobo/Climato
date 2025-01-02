5231b0964ac1ab8ca0ce70ad2f5f50d9eff33f5b
Ajay-Lobo <nishanthajay1101@gmail.com>
2025-01-02 23:14:08 +0530

    the weatherdata, forecastdata, geocodedata api created

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,43 +1,64 @@
 export interface Coordinates {
-    lat: number;
-    lon: number;
+  lat: number;
+  lon: number;
 }
 
-export interface Weather {
-    coord: Coordinates;
-    weather: {
-        id: number;
-        main: string;
-        description: string;
-        icon: string;
-    }[];
-    base: string;
-    main: {
-        temp: number;
-        feels_like: number;
-        temp_min: number;
-        temp_max: number;
-        pressure: number;
-        humidity: number;
-    };
-    visibility: number;
-    wind: {
-        speed: number;
-        deg: number;
-    };
-    clouds: {
-        all: number;
-    };
+export interface WeatherConditions {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+export interface WeatherData {
+  coord: Coordinates;
+  weather: WeatherConditions[];
+
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+
+  name: string;
+  dt: number;
+}
+
+export interface ForecastData {
+    list:Array<{
     dt: number;
-    sys: {
-        type: number;
-        id: number;
-        country: string;
-        sunrise: number;
-        sunset: number;
-    };
-    timezone: number;
-    id: number;
+    main:WeatherData['main'];
+    weather:WeatherData['weather'];
+    wind:WeatherData['wind'];
+    dt_txt:string;
+    }>;
+  city: {
     name: string;
-    cod: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+ 
+}
+
+export interface ReverseGeocodeData {
+    name:string;
+    country:string;
+    lat:number;
+    lon:number;
+    local_names?:Record<string,string>;
+    state?:string;
 }
--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,5 +1,5 @@
 import { API_CONFIG } from "./config";
-import { Coordinates } from "./types";
+import { Coordinates, ForecastData, WeatherData,ReverseGeocodeData } from "./types";
 
 class WeatherAPI {
   private createURL(endpoint: string, params: Record<string, string | number>) {
@@ -17,16 +17,35 @@ class WeatherAPI {
     return response.json();
   }
 
-  async getWeatherData({lat,lon}:Coordinates):Promise<> {
+  async getWeatherData({lat,lon}:Coordinates):Promise<WeatherData> {
     const url = this.createURL(`${API_CONFIG.BASE_URL}/weather`, {
       lat: lat.toString(),
-      lon:lat.toString(),
+      lon:lon.toString(),
       units:API_CONFIG.DEFAULT_PARAMS.units,
     });
-    return this.fetchWeatherData(url);
+    return this.fetchWeatherData<WeatherData>(url);
   }
 
-  async getForecastData() {}
 
-  async reverseGeocode() {}
+  async getForecastData({lat,lon}:Coordinates):Promise<ForecastData> {
+    const url = this.createURL(`${API_CONFIG.BASE_URL}/forecast`, {
+      lat: lat.toString(),
+      lon:lon.toString(),
+      units:API_CONFIG.DEFAULT_PARAMS.units,
+    });
+    return this.fetchWeatherData<ForecastData>(url);
+  }
+
+ 
+  async reverseGeocode({lat,lon}:Coordinates):Promise<ReverseGeocodeData[]> {
+    const url = this.createURL(`${API_CONFIG.GEO}/reverse`, {
+      lat: lat.toString(),
+      lon:lon.toString(),
+      limit:1,
+    });
+    return this.fetchWeatherData<ReverseGeocodeData[]>(url);
+  }
 }
+
+
+export const weatherAPI = new WeatherAPI();
