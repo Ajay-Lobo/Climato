420b278118f3822b23ea565deb47a5f733d54a76
Ajay-Lobo <nishanthajay1101@gmail.com>
2025-01-07 01:14:31 +0530

    small change

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Climato/',
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
